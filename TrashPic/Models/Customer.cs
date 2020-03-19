0d379c4ff48ad2945542ab2b9719f84b96cb523b
kelvin omoregie <kayo2k14@gmail.com>
2020-03-19 11:36:22 -0500

    Second commit

---
+++ TrashPic/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashPic.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
