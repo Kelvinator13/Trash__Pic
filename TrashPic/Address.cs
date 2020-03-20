651a941adcb225e7ebf5afbe0057790399dfc6d7
kelvin omoregie <kayo2k14@gmail.com>
2020-03-20 10:17:10 -0500

    Included and Address class  to further streamline functions for the database

--- TrashPic/Address.cs
+++ TrashPic/Address.cs
@@ -9,7 +9,12 @@ namespace TrashPic
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Street with Number")]
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
 
-      
+
     }
 }
--- TrashPic/EmployeeViewModel.cs
+++ TrashPic/EmployeeViewModel.cs
@@ -7,5 +7,11 @@ namespace TrashPic
 {
     public class EmployeeViewModel
     {
+        public Employee Employee { get; set; }
+        public Customer Customer { get; set; }
+        public List<Customer> Customers { get; set; }
+        public Address Address  { get; set; }
+        public MaintenanceInfo MaintenanceInfo { get; set; }
+
     }
 }
